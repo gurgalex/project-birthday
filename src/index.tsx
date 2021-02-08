ae519b61271fd1a48b6faaee3791f80406fc0b9f
Alex Gurganus <gurgalex@iastate.edu>
2021-02-07 20:57:05 -0600

    Use useRedux for state manipulation

--- src/index.tsx
+++ src/index.tsx
@@ -1,158 +1,182 @@
 import * as React from 'react';
 import ReactDOM from "react-dom";
 
-import { get, set } from 'idb-keyval';
+import {get, set} from 'idb-keyval';
 import {useEffect, useReducer, useState} from "react";
 
 import {Link, Route, Router, Switch} from "wouter";
 
 
-const enum appAction {
+const enum appStatus {
     LOADING = "Loading",
     HOME = "Home",
     SETTINGS = "Settings",
 }
 
+interface appState {
+    settings: { birthDay: Date | null }
+    status: appStatus
+    warnStorage: boolean
+}
 
-export class App extends React.Component {
-    public state: {
-        settings: {birthDay: Date},
-        status: appAction,
-    };
+const enum appActionType {
+    HOME,
+    SAVE,
+    EnableStorageFailureWarning,
+    SETTINGS,
+    SWITCH_TO_HOME,
+}
+
+interface appAction {
+    type: appActionType,
+    payload: any,
+}
+
+function appReducer(state, action: appAction) {
+    switch (action.type) {
+        case appActionType.SAVE:
+            console.log("request to save new birthDay");
+            let newDay = action.payload.birthDay;
+            console.log(newDay);
+            console.log(state);
+            return {...state, settings: {birthDay: newDay}, status: appStatus.SETTINGS};
+        case appActionType.HOME:
+            console.log("Switch to Home page");
+            return {...state, status: appStatus.HOME, settings: action.payload.settings};
+        case appActionType.SETTINGS:
+            console.log("Switch to Settings page");
+            return {...state, status: appStatus.SETTINGS};
+        case appActionType.EnableStorageFailureWarning:
+            console.warn("Writing or reading failed");
+            return {...state, warnStorage: true};
+        case appActionType.SWITCH_TO_HOME:
+            return {...state, status: appStatus.HOME};
+
+        default:
+            console.error("unhandled app action");
+            console.error(state);
+            console.error(action);
+    }
+}
+
+function getDateFromStorage(): Promise<Date | undefined> {
+    return get('date-iso').then((day: Date) => {
+        console.debug(`Got day from idb init: ${day}`);
+        return {birthDay: day};
+    }).catch(err => {
+        console.warning("Failed to get date from local storage");
+    });
+
+}
 
 
-    getDateFromStorage(): Promise<unknown> {
-        return get('date-iso').then(day => {
-            console.debug(`Got day from idb init: ${day}`);
-            return Promise.resolve(day);
+const AppNew = () => {
+    const initialAppState: appState = {settings: {birthDay: null}, status: appStatus.LOADING, warnStorage: false};
+    const [state, appDispatch] = useReducer(appReducer, initialAppState);
+
+    // load data
+    useEffect(() => {
+        getDateFromStorage().then(settings => {
+            console.log("got settings");
+            console.log(settings);
+            appDispatch({type: appActionType.HOME, payload: {settings: settings}});
         }).catch(err => {
-            console.debug("birthDay setting not set yet");
-            return Promise.resolve(null);
+            console.error(`Unhandled error when setting key with indexedDB: ${err.name}`);
+            // toggle warning state (to render warning bar)
+            // Keep default state with no data
+            appDispatch({type: 'enableStorageFailureWarning'});
+            appDispatch({type: 'Home', payload: initialAppState.settings});
+            throw err;
+            //Update status from loading -> Home
         });
+    }, []);
 
+    // render
+    console.log("app state");
+    console.log(state);
+    if (state.status === appStatus.LOADING) {
+        console.log("app in loading state");
+        return null;
     }
 
-    constructor(props) {
-        super(props);
 
-        this.state = {settings: {birthDay: null}, status: appAction.LOADING};
-        console.debug(`App State with init:`);
-        console.log(this);
-    }
+    return (
+        <>
+            {state.warnStorage && warningContent}
+            <nav>
+                <Link href="/" onClick={() => appDispatch({type: appActionType.SWITCH_TO_HOME})} componenet={AppNew}>
+                <a rel="noreferrer noopener">Home</a>
+            </Link>
+                <Link href="/#/settings" onClick={() => appDispatch({type: appActionType.SETTINGS})} componenet={AppNew}>
+                    <a rel="noreferrer noopener">Settings</a>
+                </Link>
+            </nav>
 
-    componentDidMount() {
-        this.getDateFromStorage().then(date => {
-            this.setState({settings: {birthDay: date}, status: appAction.HOME});
-        })
-    }
-
-    render() {
-        let content;
-        return (
-            <>
-                <Router hook={useHashLocation}>
-                <Switch>
-                <Route path="/settings">
-                    {() =>
-                        <>
-                            <Settings />
-                            </>
+            <Router hook={useHashLocation}>
+                    <Route path="/settings">
+                        {() => {
+                            console.log("settings router");
+                            return (
+                                <>
+                                <Settings settings={state.settings} dispatch={appDispatch}/>
+                            </>)
                         }
-                </Route>
+                        }
+                    </Route>
 
-                <Route path="/">
-                    {() => {
-                        const settings = this.state.settings
-                        return (
-                        <MainPageContent settings={settings}/>
-                        );
-                    }
-                    }
-                </Route>
+                    <Route path="/">
+                        {() => {
+                            console.log("home router");
+                            const settings = state.settings;
+                            return (
+                                <MainPageContent settings={settings}/>
+                            );
+                        }
+                        }
+                    </Route>
 
-                <Route>404, Not Found!</Route>
-                </Switch>
-                </Router>
-            </>
-        );
+                    <Route>404, Not Found!</Route>
+            </Router>
+        </>
+    );
+}
 
-        switch (this.state.status) {
-            case appAction.HOME: {
-                console.debug(`Loading HOME componenent switch`);
-                console.debug(this.state.settings);
-                let settings = this.state.settings;
-                content = <MainPageContent settings={settings}/>
-                break;
-            }
-            case appAction.SETTINGS:
-                content = <Settings birthDay={this.state.settings.birthDay} onSettingsSubmit={this.handleFormSubmit}/>;
-                break;
-            case appAction.LOADING:
-                content = null;
-        }
-
-        return (
-        <React.Fragment>
-            <h1>Hello {this.props.name}</h1>
-            {content}
-        </React.Fragment>
-        );
-    }
-
-    birthday(): Date {
-        return this.state.birthDay;
-    }
-
-    setBirthDay(date) {
-        this.setState({birthDay: date});
-     }
-
-
-    handleFormSubmit = (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        const birthDayInput = document.getElementById('set-birthday') as HTMLInputElement;
-        const birthDate = birthDayInput.valueAsDate;
-        this.setBirthDay(birthDate);
-        console.debug("updated birthDay in app");
-
-        set('date-iso', birthDate).then(res => {
-                console.debug("Set birth date in idb");
-            }
-        ).catch(err => {
-            switch (err.name) {
-                case 'QuotaExceededError':
-                    console.warn(`${err.name}: Failed to persist birthday in idb`);
-                    break;
-                default:
-                    console.error(`Unhandled error when setting key with indexedDB: ${err.name}`);
-                    throw err;
-            }
-        });
-    }
-
+const warningStyle = {
+    backgroundColor: "#9c2b2e",
+    color: "white",
 }
 
 interface appProps {
-    settings: {birthDate: Date},
+    settings: { birthDay: Date },
 }
 
+
+const warningContent = <>
+<section id="warning" style={warningStyle}><h2>Warning: Not enough space on your device</h2> <p><b>App will operate in read only mode.</b></p></section></>
+
 const MainPageContent = (props: Partial<appProps>) => {
+    console.log("main page initial props");
     console.log(props);
-    if (props.settings.birthDate) {
+    if (props.settings.birthDay) {
         return (
             <>
-                <p>Welcome your name is Alex-TBD</p>
+                <p>Welcome to the birthday reminder app!</p>
                 <p>Your birthday is: {props.settings.birthDay.toLocaleDateString()}</p>
+
+                <p>Click the below button to edit your birthday</p>
+                <Link href="/settings">
+                    <button>Setup Birthday</button>
+                </Link>
+
             </>
         )
-    }
-    else {
+    } else {
         return (
             <>
-            <p>Welcome, it looks like you haven't setup your birthday reminder yet. Click the setup button to get started.</p>
+                <p>Welcome, it looks like you haven't setup your birthday reminder yet. Click the setup button to get
+                    started.</p>
                 <Link href="/settings">
-            <button>Setup Birthday</button>
+                    <button>Setup Birthday</button>
                 </Link>
             </>
         )
@@ -160,27 +184,48 @@ const MainPageContent = (props: Partial<appProps>) => {
 }
 
 const Settings = (props) => {
-    const enum SettingsStates {
 
-    }
-    
-    const [state, dispatch] = useReducer(settingsReducer);
-
-    const birthDaySet = props.birthDay;
+    console.log("settings props");
+    console.log(props);
+    const birthDaySet = props.settings.birthDay;
     let birthDayGreeting;
     if (birthDaySet) {
-        birthDayGreeting = <p>Your birthday is {props.birthDay.toLocaleDateString()}</p>
-    }
-    else {
+        birthDayGreeting = <p>Your birthday is {props.settings.birthDay.toLocaleDateString()}</p>
+    } else {
         birthDayGreeting = <p>Set your birthday below.</p>
     }
 
     useEffect(() => {
         const settingFormElement = document.getElementById('form-settings');
-        settingFormElement.addEventListener('submit', props.onSettingsSubmit);
+        settingFormElement.addEventListener('submit', handleFormSubmit);
 
-        return () => settingFormElement.removeEventListener('submit', props.fontVariationSettings);
-    }, [props.onSettingsSubmit]);
+        return () => settingFormElement.removeEventListener('submit', handleFormSubmit);
+    }, []);
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        const birthDayInput = document.getElementById('set-birthday') as HTMLInputElement;
+        const birthDay = birthDayInput.valueAsDate;
+
+        set('date-iso', birthDay).then(res => {
+                console.debug("Set birth day in idb");
+                props.dispatch({type: appActionType.SAVE, payload: {birthDay}})
+                console.debug("updated birthDay in app");
+            }
+        ).catch(err => {
+            switch (err.name) {
+                case 'QuotaExceededError':
+                    console.warn(`${err.name}: Failed to persist birthday in idb`);
+                    props.dispatch({type: appActionType.EnableStorageFailureWarning});
+                    break;
+                default:
+                    console.error(`Unhandled error when setting key with indexedDB: ${err.name}`);
+                    console.log(JSON.stringify(err));
+                    throw err;
+            }
+        });
+    };
 
     return (
         <React.Fragment>
@@ -191,22 +236,12 @@ const Settings = (props) => {
 
                 <button type="submit">Save Settings</button>
             </form>
-        </React.Fragment>    )
-
-}
-
-const enum settingsAction {
-    LOADING = "Loading",
-    EDIT = "Edit",
-    SAVE = 'Save',
-}
-
-function settingsReducer(state, action: settingsAction) {
-
+        </React.Fragment>)
 }
 
 
-export default App;
+
+export default AppNew;
 
 // Hash routing
 // returns the current hash location in a normalized form
@@ -235,4 +270,4 @@ const useHashLocation = () => {
 const root = document.getElementById('app');
 
 // Inject our app into the DOM
-ReactDOM.render(<App name="Alex" />, root);
+ReactDOM.render(<AppNew name="Alex"/>, root);
